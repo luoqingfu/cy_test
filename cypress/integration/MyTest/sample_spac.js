5da22c9560e960367e0e1d006232afe8f505520a
luoqingfu <746832476@qq.com>
2019-02-21 10:47:53 +0800

    init

---
+++ cypress/integration/MyTest/sample_spac.js
@@ -0,0 +1,7 @@
+describe('My First Test', function(){
+    it('vist baidu,com',function(){
+        cy.visit('https://www.baidu.com')
+    })
+}
+)
+
---
+++ cypress/integration/MyTest/test_baidu.js
@@ -0,0 +1,5 @@
+describe('打开百度', function(){
+    it('successfully load', function(){
+        cy.visit('/')
+    })
+})
